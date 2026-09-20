61464dc26277ca2ba9e8da4c0c282f2d97202c6c
yhat-penguin <9972424+yhat-penguin@users.noreply.github.com>
2026-09-20 13:10:59 +0200

    autocomplete disabled

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,4 +5,20 @@ return {
       inlay_hints = { enabled = false },
     },
   },
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    opts = {
+      completion = {
+        menu = {
+          auto_show = false,
+        },
+        list = {
+          selection = {
+            preselect = false,
+          },
+        },
+      },
+    },
+  },
 }
