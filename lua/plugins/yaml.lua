8c9b9caaba9c418fc1f74d6e032b64e4be35545a
yhat-penguin <9972424+yhat-penguin@users.noreply.github.com>
2026-09-20 13:02:53 +0200

    yaml schemas added

---
+++ lua/plugins/yaml.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        yamlls = {
+          settings = {
+            yaml = {
+              validate = true,
+              format = { enable = true },
+              schemaStore = { enable = false },
+              schemas = {
+                ["https://json.schemastore.org/github-workflow.json"] = ".github/workflows/*",
+                ["https://json.schemastore.org/azure-pipelines.json"] = {
+                  "azure-pipelines.yml",
+                  "azure-pipelines/*.yml",
+                  ".azure/**/*.yml",
+                  ".azure/**/*.yaml",
+                },
+                [".databricks/bundle_config_schema.json"] = {
+                  "databricks.yml",
+                  "bundles/**/*.yml",
+                  "resources/**/*.yml",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
